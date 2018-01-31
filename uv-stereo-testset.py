ec71af9ac4f9290154871738f387b03824ca022a
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2018-01-31 19:02:57 +0700

    Add Houghman

--- uv-stereo-testset.py
+++ uv-stereo-testset.py
@@ -7,6 +7,7 @@ from __future__ import division
 import numpy as np
 import os
 import cv2
+import math
 
 from copy import copy
 
@@ -38,28 +39,38 @@ def write_ply(fn, verts, colors):
 
 def calculate_vdisparity(disp_img, max_disparity, img_height, img_width):
     # calculate v-disparity
-    vhist_vis = np.zeros((img_height, max_disp), np.float)
+    vhist_vis = np.zeros((img_height, max_disparity), np.float)
     for i in range(img_height):
         # [disp_img[i, ...]] make two dimesion arrays (just like orginal image but we just put only one row of the image here)
         # read more about calcHist at https://docs.opencv.org/2.4/modules/imgproc/doc/histograms.html
         # flatten the array to make it to one dimension array
         # divide all row with img_width value to normalize the histogram (make it less or equal to 1)
-        vhist_vis[i, ...] = cv2.calcHist(images=[disp_img[i, ...]], channels=[0], mask=None, histSize=[max_disp],
-                                         ranges=[0, max_disp]).flatten() / float(img_width)
+        vhist_vis[i, ...] = cv2.calcHist(images=[disp_img[i, ...]], channels=[0], mask=None, histSize=[max_disparity],
+                                         ranges=[0, max_disparity]).flatten() / float(img_width)
 
 
     vhist_vis = np.array(vhist_vis * 255, np.uint8)
-    vblack_mask = vhist_vis < 5
-    vhist_vis = cv2.applyColorMap(vhist_vis, cv2.COLORMAP_JET)
-    vhist_vis[vblack_mask] = 0
+    # mask_threshold = max_disparity/10
+    mask_threshold = 5
+    vblack_mask = vhist_vis < mask_threshold
+    vwhite_mask = vhist_vis > mask_threshold
+    # vhist_vis = cv2.applyColorMap(vhist_vis, cv2.COLORMAP_JET)
+    vhist_vis[vwhite_mask] = 255
+
+    # Add houghman line extract
+    # lines = cv2.HoughLinesP(vhist_vis, 1, math.pi/180.0, 10, np.array([]), 50, 10)
+    # a,b,c = lines.shape
+    # for i in range(a):
+    #     cv2.line(vhist_vis, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.LINE_AA)
+
     return vhist_vis
 
 def calculate_udisparity(disp_img, max_disparity, img_height, img_width):
     # calculate u-disparity
-    uhist_vis = np.zeros((max_disp, img_width), np.float)
+    uhist_vis = np.zeros((max_disparity, img_width), np.float)
     for i in range(img_width):
-        uhist_vis[..., i] = cv2.calcHist(images=[disp_img[..., i]], channels=[0], mask=None, histSize=[max_disp],
-                                         ranges=[0, max_disp]).flatten() / float(img_height)
+        uhist_vis[..., i] = cv2.calcHist(images=[disp_img[..., i]], channels=[0], mask=None, histSize=[max_disparity],
+                                         ranges=[0, max_disparity]).flatten() / float(img_height)
 
     uhist_vis = np.array(uhist_vis * 255, np.uint8)
     ublack_mask = uhist_vis < 5
@@ -67,13 +78,13 @@ def calculate_udisparity(disp_img, max_disparity, img_height, img_width):
     uhist_vis[ublack_mask] = 0
     return uhist_vis
 
-def scale_disparity(disparity_map, h, w):
+def scale_disparity(disparity_map, h, w, num_disp, scaling_factor):
     m = copy(disparity_map)
     for i in range(h):
         for j in range(w):
             # Invert the testset grayscale value and scale to 255 (Make the nearer point color brighter)
             # m[i][j] = (max_disp - m[i][j]) / num_disp * 255.0
-            m[i][j] = m[i][j] / num_disp * 255.0
+            m[i][j] = m[i][j] / num_disp * scaling_factor
     return m
 
 if __name__ == '__main__':
@@ -97,11 +108,12 @@ if __name__ == '__main__':
     min_disp = int(disparity_map.min())
     num_disp = max_disp-min_disp
 
-    # disparity_map = scale_disparity(disparity_map, h, w)
+    scaling_factor = 255
+    # disparity_map = scale_disparity(disparity_map, h, w, num_disp, scaling_factor)
 
     print('calculate uv-disparity...')
-    u_disparity = calculate_udisparity(disp_img=disparity_map, max_disparity=max_disp, img_height=h, img_width=w)
-    v_disparity = calculate_vdisparity(disp_img=disparity_map, max_disparity=max_disp, img_height=h, img_width=w)
+    u_disparity = calculate_udisparity(disp_img=disparity_map, max_disparity=scaling_factor, img_height=h, img_width=w)
+    v_disparity = calculate_vdisparity(disp_img=disparity_map, max_disparity=scaling_factor, img_height=h, img_width=w)
 
     scaled_disparity = cv2.applyColorMap(disparity_map, cv2.COLORMAP_JET)
     disparity_map_origin = cv2.applyColorMap(imgD, cv2.COLORMAP_JET)
