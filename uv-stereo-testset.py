1621ca1be07f9d31dc065da115c51245fb041624
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2018-02-14 11:30:15 +0700

    Add UV projection back to image

--- uv-stereo-testset.py
+++ uv-stereo-testset.py
@@ -37,7 +37,7 @@ def write_ply(fn, verts, colors):
         np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
 
 
-def calculate_vdisparity(disp_img, max_disparity, img_height, img_width):
+def calculate_vdisparity(disp_img, max_disparity, img_height, img_width, obstacle=None):
     # calculate v-disparity
     vhist_vis = np.zeros((img_height, max_disparity), np.float)
     for i in range(img_height):
@@ -51,7 +51,7 @@ def calculate_vdisparity(disp_img, max_disparity, img_height, img_width):
 
     vhist_vis = np.array(vhist_vis * 255, np.uint8)
     # mask_threshold = max_disparity/10
-    mask_threshold = 10
+    mask_threshold = 20
 
     vblack_mask = vhist_vis < mask_threshold
     vwhite_mask = vhist_vis >= mask_threshold
@@ -60,28 +60,34 @@ def calculate_vdisparity(disp_img, max_disparity, img_height, img_width):
     vhist_vis[vwhite_mask] = 255
 
     # Add houghman line extract
-    lines = cv2.HoughLinesP(vhist_vis, 1, math.pi/180.0, 5, np.array([]), 10, 10)
+    lines = cv2.HoughLinesP(vhist_vis, 1, math.pi/180.0, 5, np.array([]), 40, 10)
     a,b,c = lines.shape
     tmp = np.zeros((img_height, max_disparity), np.float)
-    obstacle = np.zeros((img_height, img_width), np.float)
     for i in range(a):
         # line on x1 y1 and x2 y2
         # x is disparity and y is row
         # print("{} {} {} {}".format(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3]))
         cv2.line(tmp, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (255, 0, 0), 1, cv2.LINE_AA)
-        expect_disp = lines[i][0][0]
-        for j in range(lines[i][0][1], lines[i][0][3]+1):
-            for k in range(img_width):
-                if disp_img[j][k] == expect_disp:
-                    obstacle[j][k] = 255
+        
+        if obstacle is None:
+            continue
+
+        if lines[i][0][0] != 0:
+            expect_disp = lines[i][0][0]
+            # for j in range row y1 to row y2
+            if expect_disp != 0:
+                for j in range(lines[i][0][1], lines[i][0][3]+1):
+                    for k in range(img_width):
+                        if disp_img[j][k] == expect_disp:
+                            obstacle[j][k] = 125
 
 
 
     vhist_vis = cv2.applyColorMap(vhist_vis, cv2.COLORMAP_JET)
     # return vhist_vis
-    return vhist_vis
+    return tmp
 
-def calculate_udisparity(disp_img, max_disparity, img_height, img_width):
+def calculate_udisparity(disp_img, max_disparity, img_height, img_width, obstacle=None):
     # calculate u-disparity
     uhist_vis = np.zeros((max_disparity, img_width), np.float)
     for i in range(img_width):
@@ -97,11 +103,23 @@ def calculate_udisparity(disp_img, max_disparity, img_height, img_width):
     uhist_vis[uwhite_mask] = 255
 
     # Add houghman line extract
-    lines = cv2.HoughLinesP(uhist_vis, 1, math.pi/180.0, 5, np.array([]), 10,15)
+    lines = cv2.HoughLinesP(uhist_vis, 1, math.pi/180.0, 5, np.array([]), 40,10)
     a,b,c = lines.shape
     tmp = np.zeros((max_disparity, img_width), np.float)
     for i in range(a):
         cv2.line(tmp, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (255, 0, 0), 1, cv2.LINE_AA)
+        # print("{} {} {} {}".format(lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3]))
+        # x is column y is disparity
+        if obstacle is None:
+            continue
+            
+        if lines[i][0][1] == lines[i][0][3] and lines[i][0][1] != 0:
+            expect_disp = lines[i][0][1]
+            # from range column x1 to column x2
+            for k in range(lines[i][0][0], lines[i][0][2]+1):
+                for j in range(img_height):
+                    if disp_img[j][k] == expect_disp:
+                        obstacle[j][k] = 125
 
     uhist_vis = cv2.applyColorMap(uhist_vis, cv2.COLORMAP_JET)
     # return uhist_vis
@@ -128,6 +146,7 @@ if __name__ == '__main__':
     #  = cv2.cvtColor(imgD, cv2.COLOR_BGR2GRAY)
 
     h, w, channel = imgD.shape
+    print('h:{} w:{} channel:{}'.format(h,w,channel))
     disparity_map = np.zeros((h, w), np.uint8)
     for i in range(h):
         for j in range(w):
@@ -139,11 +158,22 @@ if __name__ == '__main__':
     num_disp = max_disp-min_disp
 
     scaling_factor = max_disp
+    obs_u = np.zeros((h, w), np.uint8)
+    obs_v = np.zeros((h, w), np.uint8)
+    obs = np.zeros((h, w), np.uint8)
+
     disparity_map = scale_disparity(disparity_map, h, w, max_disp, num_disp, scaling_factor)
 
     print('calculate uv-disparity...')
-    u_disparity = calculate_udisparity(disp_img=disparity_map, max_disparity=scaling_factor, img_height=h, img_width=w)
-    v_disparity = calculate_vdisparity(disp_img=disparity_map, max_disparity=scaling_factor, img_height=h, img_width=w)
+    u_disparity = calculate_udisparity(disp_img=disparity_map, max_disparity=scaling_factor, img_height=h, img_width=w, obstacle=obs_u)
+    v_disparity = calculate_vdisparity(disp_img=disparity_map, max_disparity=scaling_factor, img_height=h, img_width=w, obstacle=obs_v)
+
+    obs = np.add(obs_u,obs_v)
+    x,y = obs.shape
+
+    # for i in range(x):
+    #     for j in range(y):
+    #         obs[i][j] = obs_u[i][j] + obs_v[i][j]
 
     scaled_disparity = cv2.applyColorMap(disparity_map, cv2.COLORMAP_JET)
     disparity_map_origin = cv2.applyColorMap(imgD, cv2.COLORMAP_JET)
@@ -154,5 +184,8 @@ if __name__ == '__main__':
     cv2.imshow('vdisp', v_disparity)
     cv2.imshow('original', imgR)
     cv2.imshow('original_disp', disparity_map_origin)
+    cv2.imshow('obstacle_map', obs)
+    cv2.imshow('obstacle_v',obs_v)
+    cv2.imshow('obstacle_u',obs_u)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
